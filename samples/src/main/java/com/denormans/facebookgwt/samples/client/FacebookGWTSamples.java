7839abb563b57cfb821cf7a3a60929a9fc014d22
Michael D. Norman <michael.norman@denormans.com>
2010-10-23 16:42:57 -0500

    Added starter GWT

---
+++ samples/src/main/java/com/denormans/facebookgwt/samples/client/FacebookGWTSamples.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2010 deNormans
+ * http://www.denormans.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of deNormans ("Confidential Information"). You 
+ * shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with deNormans.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * DENORMANS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.denormans.facebookgwt.samples.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * <tt>FacebookGWTSamples</tt>
+ *
+ * @author mnorman
+ * @version 1.0
+ */
+public class FacebookGWTSamples implements EntryPoint {
+  public void onModuleLoad() {
+    RootPanel.get().add(new HTML("Module Loaded..."));
+  }
+}
