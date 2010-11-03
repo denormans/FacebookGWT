ee5332d4a14cf0f9c3fd1c504ecb17cea4beaf2c
Michael D. Norman <michael.norman@denormans.com>
2010-11-02 22:34:01 -0500

    added samples

---
+++ Samples/FacebookGWTSamples/FacebookGWTSamples.nocache.js
@@ -0,0 +1,318 @@
+function FacebookGWTSamples(){
+  var $intern_0 = '', $intern_34 = '" for "gwt:onLoadErrorFn"', $intern_32 = '" for "gwt:onPropertyErrorFn"', $intern_15 = '"><\/script>', $intern_17 = '#', $intern_59 = '.cache.html', $intern_19 = '/', $intern_52 = '1558B0EF8760F54AB632129E82F7A7D0', $intern_53 = '3B983B0686982082D928EBFB35D693CF', $intern_54 = '4E37F52DB8F51F1870EB795833D4CDA8', $intern_58 = ':', $intern_26 = '::', $intern_67 = '<script defer="defer">FacebookGWTSamples.onInjectionDone(\'FacebookGWTSamples\')<\/script>', $intern_14 = '<script id="', $intern_29 = '=', $intern_18 = '?', $intern_31 = 'Bad handler "', $intern_55 = 'D009B1F3C08F3E2262FB4C99DB454E52', $intern_56 = 'DDAFE4C27EEED41F43CE58D394110802', $intern_66 = 'DOMContentLoaded', $intern_57 = 'F5CCD1298F5F139489475043082D08CE', $intern_1 = 'FacebookGWTSamples', $intern_12 = 'FacebookGWTSamples.nocache.js', $intern_25 = 'FacebookGWTSamples::', $intern_16 = 'SCRIPT', $intern_13 = '__gwt_marker_FacebookGWTSamples', $intern_20 = 'base', $intern_10 = 'baseUrl', $intern_4 = 'begin', $intern_3 = 'bootstrap', $intern_22 = 'clear.cache.gif', $intern_28 = 'content', $intern_9 = 'end', $intern_46 = 'gecko', $intern_47 = 'gecko1_8', $intern_5 = 'gwt.codesvr=', $intern_6 = 'gwt.hosted=', $intern_7 = 'gwt.hybrid', $intern_60 = 'gwt/standard/standard.css', $intern_33 = 'gwt:onLoadErrorFn', $intern_30 = 'gwt:onPropertyErrorFn', $intern_27 = 'gwt:property', $intern_65 = 'head', $intern_50 = 'hosted.html?FacebookGWTSamples', $intern_64 = 'href', $intern_45 = 'ie6', $intern_44 = 'ie8', $intern_35 = 'iframe', $intern_21 = 'img', $intern_36 = "javascript:''", $intern_61 = 'link', $intern_49 = 'loadExternalRefs', $intern_23 = 'meta', $intern_38 = 'moduleRequested', $intern_8 = 'moduleStartup', $intern_43 = 'msie', $intern_24 = 'name', $intern_40 = 'opera', $intern_37 = 'position:absolute;width:0;height:0;border:none', $intern_62 = 'rel', $intern_42 = 'safari', $intern_11 = 'script', $intern_51 = 'selectingPermutation', $intern_2 = 'startup', $intern_63 = 'stylesheet', $intern_48 = 'unknown', $intern_39 = 'user.agent', $intern_41 = 'webkit';
+  var $wnd = window, $doc = document, $stats = $wnd.__gwtStatsEvent?function(a){
+    return $wnd.__gwtStatsEvent(a);
+  }
+  :null, $sessionId = $wnd.__gwtStatsSessionId?$wnd.__gwtStatsSessionId:null, scriptsDone, loadDone, bodyDone, base = $intern_0, metaProps = {}, values = [], providers = [], answers = [], softPermutationId = 0, onLoadErrorFunc, propertyErrorFunc;
+  $stats && $stats({moduleName:$intern_1, sessionId:$sessionId, subSystem:$intern_2, evtGroup:$intern_3, millis:(new Date).getTime(), type:$intern_4});
+  if (!$wnd.__gwt_stylesLoaded) {
+    $wnd.__gwt_stylesLoaded = {};
+  }
+  if (!$wnd.__gwt_scriptsLoaded) {
+    $wnd.__gwt_scriptsLoaded = {};
+  }
+  function isHostedMode(){
+    var result = false;
+    try {
+      var query = $wnd.location.search;
+      return (query.indexOf($intern_5) != -1 || (query.indexOf($intern_6) != -1 || $wnd.external && $wnd.external.gwtOnLoad)) && query.indexOf($intern_7) == -1;
+    }
+     catch (e) {
+    }
+    isHostedMode = function(){
+      return result;
+    }
+    ;
+    return result;
+  }
+
+  function maybeStartModule(){
+    if (scriptsDone && loadDone) {
+      var iframe = $doc.getElementById($intern_1);
+      var frameWnd = iframe.contentWindow;
+      if (isHostedMode()) {
+        frameWnd.__gwt_getProperty = function(name){
+          return computePropValue(name);
+        }
+        ;
+      }
+      FacebookGWTSamples = null;
+      frameWnd.gwtOnLoad(onLoadErrorFunc, $intern_1, base, softPermutationId);
+      $stats && $stats({moduleName:$intern_1, sessionId:$sessionId, subSystem:$intern_2, evtGroup:$intern_8, millis:(new Date).getTime(), type:$intern_9});
+    }
+  }
+
+  function computeScriptBase(){
+    if (metaProps[$intern_10]) {
+      base = metaProps[$intern_10];
+      return base;
+    }
+    var thisScript;
+    var scriptTags = $doc.getElementsByTagName($intern_11);
+    for (var i = 0; i < scriptTags.length; ++i) {
+      if (scriptTags[i].src.indexOf($intern_12) != -1) {
+        thisScript = scriptTags[i];
+      }
+    }
+    if (!thisScript) {
+      var markerId = $intern_13;
+      var markerScript;
+      $doc.write($intern_14 + markerId + $intern_15);
+      markerScript = $doc.getElementById(markerId);
+      thisScript = markerScript && markerScript.previousSibling;
+      while (thisScript && thisScript.tagName != $intern_16) {
+        thisScript = thisScript.previousSibling;
+      }
+    }
+    function getDirectoryOfFile(path){
+      var hashIndex = path.lastIndexOf($intern_17);
+      if (hashIndex == -1) {
+        hashIndex = path.length;
+      }
+      var queryIndex = path.indexOf($intern_18);
+      if (queryIndex == -1) {
+        queryIndex = path.length;
+      }
+      var slashIndex = path.lastIndexOf($intern_19, Math.min(queryIndex, hashIndex));
+      return slashIndex >= 0?path.substring(0, slashIndex + 1):$intern_0;
+    }
+
+    ;
+    if (thisScript && thisScript.src) {
+      base = getDirectoryOfFile(thisScript.src);
+    }
+    if (base == $intern_0) {
+      var baseElements = $doc.getElementsByTagName($intern_20);
+      if (baseElements.length > 0) {
+        base = baseElements[baseElements.length - 1].href;
+      }
+       else {
+        base = getDirectoryOfFile($doc.location.href);
+      }
+    }
+     else if (base.match(/^\w+:\/\//)) {
+    }
+     else {
+      var img = $doc.createElement($intern_21);
+      img.src = base + $intern_22;
+      base = getDirectoryOfFile(img.src);
+    }
+    if (markerScript) {
+      markerScript.parentNode.removeChild(markerScript);
+    }
+    return base;
+  }
+
+  function processMetas(){
+    var metas = document.getElementsByTagName($intern_23);
+    for (var i = 0, n = metas.length; i < n; ++i) {
+      var meta = metas[i], name = meta.getAttribute($intern_24), content;
+      if (name) {
+        name = name.replace($intern_25, $intern_0);
+        if (name.indexOf($intern_26) >= 0) {
+          continue;
+        }
+        if (name == $intern_27) {
+          content = meta.getAttribute($intern_28);
+          if (content) {
+            var value, eq = content.indexOf($intern_29);
+            if (eq >= 0) {
+              name = content.substring(0, eq);
+              value = content.substring(eq + 1);
+            }
+             else {
+              name = content;
+              value = $intern_0;
+            }
+            metaProps[name] = value;
+          }
+        }
+         else if (name == $intern_30) {
+          content = meta.getAttribute($intern_28);
+          if (content) {
+            try {
+              propertyErrorFunc = eval(content);
+            }
+             catch (e) {
+              alert($intern_31 + content + $intern_32);
+            }
+          }
+        }
+         else if (name == $intern_33) {
+          content = meta.getAttribute($intern_28);
+          if (content) {
+            try {
+              onLoadErrorFunc = eval(content);
+            }
+             catch (e) {
+              alert($intern_31 + content + $intern_34);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function unflattenKeylistIntoAnswers(propValArray, value){
+    var answer = answers;
+    for (var i = 0, n = propValArray.length - 1; i < n; ++i) {
+      answer = answer[propValArray[i]] || (answer[propValArray[i]] = []);
+    }
+    answer[propValArray[n]] = value;
+  }
+
+  function computePropValue(propName){
+    var value = providers[propName](), allowedValuesMap = values[propName];
+    if (value in allowedValuesMap) {
+      return value;
+    }
+    var allowedValuesList = [];
+    for (var k in allowedValuesMap) {
+      allowedValuesList[allowedValuesMap[k]] = k;
+    }
+    if (propertyErrorFunc) {
+      propertyErrorFunc(propName, allowedValuesList, value);
+    }
+    throw null;
+  }
+
+  var frameInjected;
+  function maybeInjectFrame(){
+    if (!frameInjected) {
+      frameInjected = true;
+      var iframe = $doc.createElement($intern_35);
+      iframe.src = $intern_36;
+      iframe.id = $intern_1;
+      iframe.style.cssText = $intern_37;
+      iframe.tabIndex = -1;
+      $doc.body.appendChild(iframe);
+      $stats && $stats({moduleName:$intern_1, sessionId:$sessionId, subSystem:$intern_2, evtGroup:$intern_8, millis:(new Date).getTime(), type:$intern_38});
+      iframe.contentWindow.location.replace(base + initialHtml);
+    }
+  }
+
+  providers[$intern_39] = function(){
+    var ua = navigator.userAgent.toLowerCase();
+    var makeVersion = function(result){
+      return parseInt(result[1]) * 1000 + parseInt(result[2]);
+    }
+    ;
+    if (ua.indexOf($intern_40) != -1) {
+      return $intern_40;
+    }
+     else if (ua.indexOf($intern_41) != -1) {
+      return $intern_42;
+    }
+     else if (ua.indexOf($intern_43) != -1) {
+      if (document.documentMode >= 8) {
+        return $intern_44;
+      }
+       else {
+        var result = /msie ([0-9]+)\.([0-9]+)/.exec(ua);
+        if (result && result.length == 3) {
+          var v = makeVersion(result);
+          if (v >= 6000) {
+            return $intern_45;
+          }
+        }
+      }
+    }
+     else if (ua.indexOf($intern_46) != -1) {
+      var result = /rv:([0-9]+)\.([0-9]+)/.exec(ua);
+      if (result && result.length == 3) {
+        if (makeVersion(result) >= 1008)
+          return $intern_47;
+      }
+      return $intern_46;
+    }
+    return $intern_48;
+  }
+  ;
+  values[$intern_39] = {gecko:0, gecko1_8:1, ie6:2, ie8:3, opera:4, safari:5};
+  FacebookGWTSamples.onScriptLoad = function(){
+    if (frameInjected) {
+      loadDone = true;
+      maybeStartModule();
+    }
+  }
+  ;
+  FacebookGWTSamples.onInjectionDone = function(){
+    scriptsDone = true;
+    $stats && $stats({moduleName:$intern_1, sessionId:$sessionId, subSystem:$intern_2, evtGroup:$intern_49, millis:(new Date).getTime(), type:$intern_9});
+    maybeStartModule();
+  }
+  ;
+  processMetas();
+  computeScriptBase();
+  var strongName;
+  var initialHtml;
+  if (isHostedMode()) {
+    if ($wnd.external && ($wnd.external.initModule && $wnd.external.initModule($intern_1))) {
+      $wnd.location.reload();
+      return;
+    }
+    initialHtml = $intern_50;
+    strongName = $intern_0;
+  }
+  $stats && $stats({moduleName:$intern_1, sessionId:$sessionId, subSystem:$intern_2, evtGroup:$intern_3, millis:(new Date).getTime(), type:$intern_51});
+  if (!isHostedMode()) {
+    try {
+      unflattenKeylistIntoAnswers([$intern_46], $intern_52);
+      unflattenKeylistIntoAnswers([$intern_47], $intern_53);
+      unflattenKeylistIntoAnswers([$intern_45], $intern_54);
+      unflattenKeylistIntoAnswers([$intern_40], $intern_55);
+      unflattenKeylistIntoAnswers([$intern_42], $intern_56);
+      unflattenKeylistIntoAnswers([$intern_44], $intern_57);
+      strongName = answers[computePropValue($intern_39)];
+      var idx = strongName.indexOf($intern_58);
+      if (idx != -1) {
+        softPermutationId = Number(strongName.substring(idx + 1));
+        strongName = strongName.substring(0, idx);
+      }
+      initialHtml = strongName + $intern_59;
+    }
+     catch (e) {
+      return;
+    }
+  }
+  var onBodyDoneTimerId;
+  function onBodyDone(){
+    if (!bodyDone) {
+      bodyDone = true;
+      if (!__gwt_stylesLoaded[$intern_60]) {
+        var l = $doc.createElement($intern_61);
+        __gwt_stylesLoaded[$intern_60] = l;
+        l.setAttribute($intern_62, $intern_63);
+        l.setAttribute($intern_64, base + $intern_60);
+        $doc.getElementsByTagName($intern_65)[0].appendChild(l);
+      }
+      maybeStartModule();
+      if ($doc.removeEventListener) {
+        $doc.removeEventListener($intern_66, onBodyDone, false);
+      }
+      if (onBodyDoneTimerId) {
+        clearInterval(onBodyDoneTimerId);
+      }
+    }
+  }
+
+  if ($doc.addEventListener) {
+    $doc.addEventListener($intern_66, function(){
+      maybeInjectFrame();
+      onBodyDone();
+    }
+    , false);
+  }
+  var onBodyDoneTimerId = setInterval(function(){
+    if (/loaded|complete/.test($doc.readyState)) {
+      maybeInjectFrame();
+      onBodyDone();
+    }
+  }
+  , 50);
+  $stats && $stats({moduleName:$intern_1, sessionId:$sessionId, subSystem:$intern_2, evtGroup:$intern_3, millis:(new Date).getTime(), type:$intern_9});
+  $stats && $stats({moduleName:$intern_1, sessionId:$sessionId, subSystem:$intern_2, evtGroup:$intern_49, millis:(new Date).getTime(), type:$intern_4});
+  $doc.write($intern_67);
+}
+
+FacebookGWTSamples();
